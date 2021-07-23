4c7a1969092edd0062999ab37e664de70aef94a5
Sunita Nhemafuki <snhemafu@purdue.edu>
2021-07-22 20:20:10 -0400

    Initial Lesson 1

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Sunita Nhemafuki on 7/22/21.
+//  Copyright © 2021 Sunita Nhemafuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
